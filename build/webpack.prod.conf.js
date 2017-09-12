80d3e4a51e0080a6ea594a0b91236ca4759bce57
dannyvaughton <danny.vaughton@gmail.com>
2017-09-12 10:41:29 +0100

    Fixing incorrect asset paths

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -92,7 +92,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         //from: path.resolve(__dirname, '../static'),
         //to: config.build.assetsSubDirectory,
         //ignore: ['.*']
-        from: path.resolve(__dirname, '../src'),
+        from: path.resolve(__dirname, '../static'),
       }
     ]),
     /*
